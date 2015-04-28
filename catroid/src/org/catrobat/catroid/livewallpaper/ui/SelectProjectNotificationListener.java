9ba99d8a29dd31b6a16f65e6c5b276220a0249e8
Marco Rauscher <marco.rauscher@student.tugraz.at>
2015-04-28 13:36:18 +0200

    Added default LWP (when no PocketCode project is loaded)

---
+++ catroid/src/org/catrobat/catroid/livewallpaper/ui/SelectProjectNotificationListener.java
@@ -0,0 +1,115 @@
+/*
+ * Catroid: An on-device visual programming system for Android devices
+ * Copyright (C) 2010-2015 The Catrobat Team
+ * (<http://developer.catrobat.org/credits>)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * An additional term exception under section 7 of the GNU Affero
+ * General Public License, version 3, is available at
+ * http://developer.catrobat.org/license_additional_term
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.catrobat.catroid.livewallpaper.ui;
+
+import android.os.SystemClock;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.Align;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+import org.catrobat.catroid.common.Constants;
+import org.catrobat.catroid.content.Sprite;
+import org.catrobat.catroid.io.SoundManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by marco on 28.04.15.
+ */
+public class SelectProjectNotificationListener implements ApplicationListener {
+
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	private Texture texture;
+	private float w,h;
+	private BitmapFont font;
+
+	public SelectProjectNotificationListener() {
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1f);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		batch.setProjectionMatrix(camera.combined);
+
+		batch.begin();
+		font.draw(batch, "Press Settings to load project!", w/2-font.getBounds("Press Settings to load project!").width/2,
+															h/2);
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		font.dispose();
+		batch.dispose();
+	}
+
+	@Override
+	public void create() {
+
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,w,h);
+		batch = new SpriteBatch();
+
+		font = new BitmapFont();
+		font.setColor(Color.WHITE);
+		font.setScale(2f);
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+
+}
